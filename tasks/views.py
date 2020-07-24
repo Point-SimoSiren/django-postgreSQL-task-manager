152875ac8cc31b655a8a670a811dd1cc42891cf0
Simo Siren <simo.siren@stud.pointcollege.fi>
2020-07-24 11:51:42 +0300

    bootstrap, fas implemented. input field done

--- tasks/views.py
+++ tasks/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse # itse lisätty import
 # Create your views here.
 
 def task_listing(request):
-    return HttpResponse('<h2>hello from task listing view</h2>')
+    return render(request,'tasks/task_list.html')
 
 def tietoa(request):
     return HttpResponse('<h3>Django & postgreSQL app</h3>')
